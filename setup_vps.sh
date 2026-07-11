b4261719f1c6139c6f4c880c5a10809542dc8c0b
SkyRent Developer <imran.yasin3964@gmail.com>
2026-07-11 17:20:41 +0500

    fix(vps): add libavdevice-dev and libpostproc-dev to setup_vps.sh to fix avdevice.h compilation error

--- setup_vps.sh
+++ setup_vps.sh
@@ -17,7 +17,7 @@ apt-get update -qq
 apt-get install -y -qq software-properties-common
 add-apt-repository -y ppa:ubuntuhandbook1/ffmpeg7
 apt-get update -qq
-apt-get install -y -qq build-essential python3 make g++ ffmpeg libavcodec-dev libavformat-dev libavutil-dev libavfilter-dev libswresample-dev libswscale-dev
+apt-get install -y -qq build-essential python3 make g++ ffmpeg libavcodec-dev libavformat-dev libavutil-dev libavfilter-dev libswresample-dev libswscale-dev libavdevice-dev libpostproc-dev
 
 # 2. Rebuild node-av from source.
 #    Since node-av npm packages omit binding.gyp (prebuilt only), we must download
